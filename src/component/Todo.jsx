f4ae093fb712484e3edb0e11d4cb9e7b283948e3
Ameer Ameen <ameerapex496@gmail.com>
2024-02-16 01:30:58 +0100

    added html tree

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,5 +1,28 @@
 import React from 'react'
 
 export default function Todo() {
-  return <div>Todo</div>
+  return (
+    <>
+      <div>
+        <h1>todos</h1>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <p>I WILL DANCE</p>
+          </li>
+          <li>
+            <p>I WILL fly</p>
+          </li>
+          <li>
+            <p>I WILL fight</p>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <input type="text" />
+        <button>Add</button>
+      </div>
+    </>
+  )
 }
