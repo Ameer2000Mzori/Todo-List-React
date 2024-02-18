c0ccbffaf58e8bc3ae854e6f1ec2f942b654f668
Ameer Ameen <ameerapex496@gmail.com>
2024-02-19 00:39:59 +0100

    added more logic like clicking the todos

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -4,16 +4,26 @@ export default function Todo() {
   const [todos, setTodos] = useState([])
 
   // use effect for the first render
+
   useEffect(() => {
     setTodos(todoList)
   }, [])
 
   const addTodo = () => {
     const newTodo = {
-      tet: 'this is a new todo',
+      title: 'this is a new todo',
       completed: false,
     }
+
+    setTodos((todos) => [...todos, newTodo])
   }
+
+  // here each todo is clicked will become true or false
+  const finishedTodo = (e) => {
+    const foundTodo = todos.findIndex((todo) => todo.title === e.textContent)
+    todos[foundTodo].completed = true
+  }
+
   return (
     <>
       <div>
@@ -22,7 +32,11 @@ export default function Todo() {
       <div>
         <ul>
           {todos.map((item, index) => {
-            return <li key={index}>{item.title}</li>
+            return (
+              <li onClick={(e) => finishedTodo(e.target)} key={index}>
+                {item.title}
+              </li>
+            )
           })}
         </ul>
       </div>
