42b891c093f71270edaf0ded82dffa88dcfa3f6f
Ameer Ameen <ameerapex496@gmail.com>
2024-02-19 03:53:56 +0100

    finished testing the whole app

--- cypress/component/Todo.cy.jsx
+++ cypress/component/Todo.cy.jsx
@@ -6,20 +6,27 @@ describe('Todo.cy.jsx', () => {
   })
   it('should add new todo', () => {
     cy.mount(<Todo />)
-    cy.get('input').type('text')
+    cy.get('input').type('test')
     cy.get('button').contains('Add').click()
     cy.get('input').clear()
     cy.get('input').should('have.value', '')
   })
   it('should remove todo', () => {
     cy.mount(<Todo />)
-    cy.get('input').type('text')
+
     cy.get('button').eq(1).contains('remove').click()
   })
   it('should done and undo ', () => {
     cy.mount(<Todo />)
-    cy.get('input').type('text')
+
     cy.get('button').eq(0).contains('done').click()
     cy.get('button').eq(0).contains(' undo').click()
+  })
+  it('should edit todo ', () => {
+    cy.mount(<Todo />)
+    cy.get('button').eq(2).contains('edit').click()
+    cy.get('input').eq(0).clear()
+    cy.get('input').eq(0).type('test edit')
+    cy.get('button').eq(0).contains('Done').click()
   })
 })
